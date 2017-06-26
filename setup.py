304d6e7da4aae9f421a9abdf912014bc36b41152
canicio <canicio7@gmail.com>
2017-06-26 11:59:18 +0200

    Delete method __raise_thread_with_window() in api

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(name='Graphtiny',
-      version='1.0.0',
+      version='0.0.1',
       description='A Python library that lets you display graphics in streaming, simply and easily. Especially suitable for scientific use. It Is an abstraction layer for the PyQtGraph library',
       author='Carlos Canicio',
       author_email='canicio@lcc.uma.es',
+      url='https://github.com/canicio/graphtiny',
+      download_url='https://github.com/canicio/graphtiny/archive/0.0.1.tar.gz',
       maintainer='Carlos Canicio',
       maintainer_email='canicio@lcc.uma.es',
       license='MIT (Massachusetts Institute of Technology)',
       packages=['graphtiny'],
       # packages=find_packages(),
+      keywords=['graph', 'ui', 'chart', 'plot'],
+      classifiers=[],
       )
